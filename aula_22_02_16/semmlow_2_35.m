2a0ef2c7e9ff2d6dd4e1758a43fd426138cd7de3
Vinícius Almeida <almeidabarrosdf@gmail.com>
2022-02-21 10:42:27 -0300

    fixed 2_24

--- aula_22_02_16/semmlow_2_35.m
+++ aula_22_02_16/semmlow_2_35.m
@@ -4,7 +4,7 @@ Ts = t_med(2) - t_med(1); % Calculates sample period
 [cov_pre,lags_pre] = axcor(hr_med - mean(hr_med)); % Auto-covariance
 % ------------- Graph plotting --------------------------
 hold on
-plot(lags_pre*Ts,cov_pre,'k'); hold on; 
+plot(lags_pre,cov_pre,'k'); hold on; 
 axis([-30 30 -0.2 1.2]);
 ylabel('$r_{xy}$','Interpreter','latex', 'Fontsize', 18)
 xlabel('Lags (sec)');
--- lista_02/semmlow_2_24.m
+++ lista_02/semmlow_2_24.m
@@ -1,11 +1,6 @@
 close all; clear variables; clc;
-Ts = 0.01; % Sample interval
-Tt = 3; % Total time
-N = Tt/Ts; % Number of points
-t = (0:N-1)*Ts; % Time vector
-f = 1.5; % Frequency of both signals 
-x = sin(2*pi*f*t); % Sine wave
-y = cos(2*pi*f*t); % Cossine wave
+load('correl1.mat') % load x and y signals
+N = length(x); % Number of points
 rxy_p = sum((x-mean(x)).*(y-mean(y))) / ((N-1)*sqrt(var(x)*var(y))); % Pearson correlation
 % --------- Log Command Window text to file --------------------------
 filename = sprintf('%s.txt',mfilename);
